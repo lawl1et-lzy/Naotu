2cdb43e2a25832899e210c9fdb75415f38615044
林志勇 <linzhiyong@changingedu.com>
2019-08-06 10:33:22 +0800

    【app】1. 修改打包配置 2. 添加browerHistory 3. 修改图片路径问题

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -78,7 +78,8 @@ export const constantRoutes = [
 ]
 
 const createRouter = () => new Router({
-  // mode: 'history', // require service support
+  mode: 'history', // require service support
+  base: 'naotu',
   scrollBehavior: () => ({ y: 0 }),
   routes: constantRoutes
 })
