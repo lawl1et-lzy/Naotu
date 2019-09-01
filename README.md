716e25455b5ed0a19fe55d9f3575759b028a84f1
林志勇 <linzhiyong@changingedu.com>
2019-09-02 00:06:29 +0800

    【server】1. 删除ssl代码 2. 修改README

--- README.md
+++ README.md
@@ -2,11 +2,13 @@
 
 ```
 ---
- |---- app/                      客户端            
- |---- server/                   服务端     
+ |---- app/                            客户端            
+ |---- server/                         服务端     
+    |---- api/                         接口层           
     |---- config/                      数据库配置项            
-    |---- controler/                   接口层        
-    |---- db/                          模型层和连接池      
+    |---- controler/                   业务层        
+    |---- dao/                         数据库操作层        
+    |---- model/                       模型层      
     |---- public/                      公共资源     
     |---- util/                        工具类       
     |---- package.json                 配置信息            
--- server/app.js
+++ server/app.js
@@ -6,7 +6,6 @@ const fs = require('fs'); // 引入文件模块
 const logger = require('morgan'); // 日志组件
 const api = require('./api/index.js'); // 引入api
 const http = require('http');
-// const https = require('https');
 const db =  require('./config/connect.js'); // mongodb 配置
 const validateMW = require('./util/validate.js') // 验证中间件
 const sessionCfg = require('./config/session.js') // session 配置
@@ -14,11 +13,6 @@ const app = express();
 
 db.openDB();  // mongodb 启动
 const PORT = 3000; // 端口号
-// const SSLPORT = 3001;
-// var certOptions = {
-//   key: fs.readFileSync('ssl/server.key','utf8'),
-//   cert: fs.readFileSync('ssl/server.crt','utf8')
-// }
 
 app.use(session(sessionCfg));
 app.use(logger('dev'));
@@ -31,26 +25,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(validateMW.validateSession)
 
 app.use(api);
-//设置跨域访问
-// app.all('*', function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", req.headers.origin)
-//   res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild");
-//   res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-//   res.header("X-Powered-By",'3.2.1')
-//   res.header("Content-Type", "application/json;charset=utf-8");
-//   // 允许跨域cookie存储
-//   res.header("Access-Control-Allow-Credentials", "true")
-//   if(req.method=="OPTIONS") res.sendStatus(200);/*让options请求快速返回*/
-//   else  next();
-// });
 
 const httpServer = http.createServer(app);
-// const httpsServer = https.createServer(certOptions, app);
 
 httpServer.listen(PORT);
-// httpsServer.listen(SSLPORT);
 
 console.log('success listen http server…………', PORT);
-// console.log('success listen https server…………', SSLPORT);
 
 module.exports = app;
